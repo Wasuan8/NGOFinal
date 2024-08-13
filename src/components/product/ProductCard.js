e05f37becc94ef786df6c1028207f00d61d979d5
Wasim <wasim@forealproperty.com.au>
2024-08-13 18:25:34 +0530

    Added Api

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -1,69 +1,72 @@
-import React, { useContext } from 'react';
-import { IoMdStar } from 'react-icons/io';
-import { Link } from 'react-router-dom';
-import { displayMoney } from '../../helpers/utils';
-import cartContext from '../../contexts/cart/cartContext';
-import useActive from '../../hooks/useActive';
+import React, { useEffect, useState, useContext } from "react";
+import { IoMdStar } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { displayMoney } from "../../helpers/utils";
+import cartContext from "../../contexts/cart/cartContext";
+import useActive from "../../hooks/useActive";
 
+const ProductCard = () => {
+  const [products, setProducts] = useState([]);
+  const { addItem } = useContext(cartContext);
+  const { active, handleActive, activeClass } = useActive(false);
 
-const ProductCard = (props) => {
+  useEffect(() => {
+    fetch("https://ngo-server-xyum.onrender.com/api/products?mode=admin", { cache: "no-store" })
+      .then((res) => res.json())
+      .then((data) => {
+        // Assuming data is an array, add the product object to the array
+        setProducts(data);
+      });
+  }, []);
 
-    const { id, images, title, info, finalPrice, originalPrice, rateCount, path } = props;
+  const handleAddItem = (product) => {
+    addItem(product);
+    handleActive(product.id);
+    setTimeout(() => {
+      handleActive(false);
+    }, 3000);
+  };
 
-    const { addItem } = useContext(cartContext);
-    const { active, handleActive, activeClass } = useActive(false);
+  return (
+    <div className="products-container">
+      {products.map((product) => {
+        const newPrice = displayMoney(product.price);
+        const oldPrice = displayMoney(product.originalPrice || product.price); // Assuming originalPrice might be in the API or use the same price
+        const productRating = [...Array(product.rating)].map((_, i) => <IoMdStar key={i} />);
 
-
-    // handling Add-to-cart
-    const handleAddItem = () => {
-        const item = { ...props };
-        addItem(item);
-
-        handleActive(id);
-
-        setTimeout(() => {
-            handleActive(false);
-        }, 3000);
-    };
-
-    const newPrice = displayMoney(finalPrice);
-    const oldPrice = displayMoney(originalPrice);
-
-
-    return (
-        <>
-            <div className="card products_card">
-                <figure className="products_img">
-                    <Link to={`${path}${id}`}>
-                        <img src={images[0]} alt="product-img" />
-                    </Link>
-                </figure>
-                <div className="products_details">
-                    <span className="rating_star">
-                        {
-                            [...Array(rateCount)].map((_, i) => <IoMdStar key={i} />)
-                        }
-                    </span>
-                    <h3 className="products_title">
-                        <Link to={`${path}${id}`}>{title}</Link>
-                    </h3>
-                    <h5 className="products_info">{info}</h5>
-                    <div className="separator"></div>
-                    <h2 className="products_price">
-                        {newPrice} &nbsp;
-                        <small><del>{oldPrice}</del></small>
-                    </h2>
-                    <button
-                        type="button"
-                        className={`btn products_btn ${activeClass(id)}`}
-                        onClick={handleAddItem}
-                    >
-                        {active ? 'Added' : 'Add to cart'}
-                    </button>
-                </div>
+        return (
+          <div className="card products_card" key={product.id}>
+            <figure className="products_img">
+              <Link to={`/products/${product.slug}`}>
+                <img src={product.mainImage ? `/${product.mainImage}` : "/product_placeholder.jpg"} alt={product.title} />
+              </Link>
+            </figure>
+            <div className="products_details">
+              <span className="rating_star">
+                {productRating}
+              </span>
+              <h3 className="products_title">
+                <Link to={`/products/${product.slug}`}>{product.title}</Link>
+              </h3>
+              <h5 className="products_info">{product.description}</h5>
+              <div className="separator"></div>
+              <h2 className="products_price">
+                {newPrice} &nbsp;
+                {product.originalPrice && <small><del>{oldPrice}</del></small>}
+              </h2>
+              <button
+                type="button"
+                className={`btn products_btn ${activeClass(product.id)}`}
+                onClick={() => handleAddItem(product)}
+              >
+                {active ? "Added" : "Add to cart"}
+              </button>
             </div>
-        </>
-    );
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ProductCard;
